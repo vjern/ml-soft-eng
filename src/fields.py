0bf4acab151a368cf30d138bb08b6dbd50027b8e
vjern <jocelyn.vernay@gmail.com>
2023-11-30 18:39:49 +0100

    Add field meta & examples management

--- src/fields.py
+++ src/fields.py
@@ -1,10 +1,11 @@
 import json
 from dataclasses import dataclass
 from typing import Optional
+from collections import defaultdict
 
 
 @dataclass
-class ExtractField:
+class Field:
     name: str
     multiple: bool
     label: str
@@ -13,9 +14,28 @@ class ExtractField:
     qa_pipeline: Optional[str] = None
 
 
-def from_json(path: str) -> dict[str, ExtractField]:
+@dataclass
+class Example:
+    product_description: str
+    output: str
+
+
+def fields_metadata_from_json(path: str) -> dict[str, Field]:
     registry = {}
     with open(path) as f:
         for field in json.load(f):
-            registry[field['name']] = ExtractField(**field)
+            registry[field['name']] = Field(**field)
     return registry
+
+
+def examples_from_json(path: str) -> dict[str, list[Example]]:
+    coll = defaultdict(list)
+    with open(path) as f:
+        for example in json.load(f):
+            coll[example['field_name']].append(
+                Example(
+                    output=example['field_value'],
+                    product_description=example['LIBL_LIBELLE'],
+                )
+            )
+    return coll
